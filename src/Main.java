b24c4d95fc9c5a4d9a85b9064428949ccb105261
Sergei Kerencev <Kerentzev@bk.ru>
2022-04-05 22:26:13 +0300

    Дз готово

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 7, 8, 9, 10};
+
+        int number = MissedNumber.find(arr);
+
+        System.out.println(number);
+    }
+}
